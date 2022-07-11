83e666d34cc95b210ccd993838be327d84a6c9ef
Dari Alves <darieltondf@gmail.com>
2022-07-11 20:22:25 -0300

    swift function

---
+++ playground/swift01.playground/Contents.swift
@@ -0,0 +1,18 @@
+//func sum (x: Int, y: Int) -> Int {
+//    let result = x + y
+//    return result
+//}
+//
+//let result = sum(x: 10, y: 5)
+
+func sum(_ x: Int, _ y: Int) -> Int {
+    let result = x + y
+    return result
+}
+
+let result = sum(10, 5)
+print(result)
+
+
+
+
